89ab4c98a3b56ace320925296145d0e2fc9077b6
wowsant <sant.clair@outlook.com>
2017-11-21 00:44:30 -0200

    Alterações python

--- python/index.py
+++ python/index.py
@@ -1,24 +1,34 @@
 import botometer
-#import MySQLdb # para o MySQL
+import MySQLdb
 
-#db = MySQLdb.connect(host="localhost", user="root", passwd="", db="twitter")
-#cursor = db.cursor()
+# conexão com banco
+db = MySQLdb.connect(host="localhost", user="root", passwd="", db="twitter")
+cursor = db.cursor()
 
-#cursor.execute("SELECT * FROM relacionamento")
-# Conta o numero de linhas na tabela
-#numrows = int(cursor.rowcount)
+cursor.execute("SELECT id_user FROM relacionamento")
+data = cursor.fetchall()
+array = []
+user = []
+y = 0
+for val in range(len(data)):
+    cursor.execute("SELECT id_user FROM botometer WHERE id_user = %s", data[val])
+    aux = cursor.fetchall()
+    if not aux:
+        for x in data[val]:
+            if y != x:
+                y = x
+                user.append(y)
 
-#print(numrows)
 mashape_key = "UtsZRmBoUemshJJgNodC6470QQmvp1ipISfjsnqmNnrtwbplzB"
 twitter_app_auth = {
     'consumer_key': 'oBl2W04bnG1PVXNvGqwiKyb4Q',
     'consumer_secret': 'NNcZEyyvEJEaMnYsKCc3fdhqAgHBuq2NkWQyAAawk7gAG4hJBZ',
     'access_token': '313648213-xuPWDguAhDMGGVAlpiqw0dPzlFhyKtBvgZoQrgNJ',
     'access_token_secret': 'Fxja3YODhDe80YbtQefXAW96yOYM1WcO7lOQh4Ahna9zr',
-  }
-
+}
 bom = botometer.Botometer(wait_on_ratelimit=True,  mashape_key=mashape_key,   **twitter_app_auth)
 
-# Check a single account
-result = bom.check_account('@wowsant')
-print(result)
+for result in bom.check_accounts_in(user):
+    print(result)
+    cursor.execute("INSERT INTO json_botometer (json_extra) VALUES (%s)", [str(result)])
+    db.commit()
