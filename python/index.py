21be7ffe92877aa9bf8e54f147f7e34a72fb1d07
wowsant <sant.clair@outlook.com>
2017-11-21 22:59:59 -0200

    Python

--- python/index.py
+++ python/index.py
@@ -1,7 +1,6 @@
 import botometer
 import MySQLdb
 
-# conexão com banco
 db = MySQLdb.connect(host="localhost", user="root", passwd="", db="twitter")
 cursor = db.cursor()
 
@@ -10,8 +9,9 @@ data = cursor.fetchall()
 array = []
 user = []
 y = 0
+
 for val in range(len(data)):
-    cursor.execute("SELECT id_user FROM botometer WHERE id_user = %s", data[val])
+    cursor.execute("SELECT id_user FROM json_botometer   WHERE id_user = %s", data[val])
     aux = cursor.fetchall()
     if not aux:
         for x in data[val]:
@@ -28,7 +28,6 @@ twitter_app_auth = {
 }
 bom = botometer.Botometer(wait_on_ratelimit=True,  mashape_key=mashape_key,   **twitter_app_auth)
 
-for result in bom.check_accounts_in(user):
-    print(result)
-    cursor.execute("INSERT INTO json_botometer (json_extra) VALUES (%s)", [str(result)])
+for id_user, json in bom.check_accounts_in(user):
+    cursor.execute("INSERT INTO json_botometer (id_user, json_extra) VALUES (%s, %s)", [str(id_user), str(json)])
     db.commit()
