6fe087d940e6d3d2ab42383b6f7d5adde4ae5bcb
wowsant <sant.clair@outlook.com>
2017-11-24 23:06:38 -0200

    add

--- python/index.py
+++ python/index.py
@@ -4,7 +4,7 @@ import MySQLdb
 db = MySQLdb.connect(host="localhost", user="root", passwd="", db="twitter")
 cursor = db.cursor()
 
-cursor.execute("SELECT id_user FROM relacionamento")
+cursor.execute("SELECT id_seguidor FROM relacionamento")
 data = cursor.fetchall()
 array = []
 user = []
@@ -18,6 +18,9 @@ for val in range(len(data)):
             if y != x:
                 y = x
                 user.append(y)
+                print(y, 'Este vamos gravar')
+    else:
+        print(aux, 'Este já esta gravado')
 
 mashape_key = "UtsZRmBoUemshJJgNodC6470QQmvp1ipISfjsnqmNnrtwbplzB"
 twitter_app_auth = {
@@ -29,5 +32,7 @@ twitter_app_auth = {
 bom = botometer.Botometer(wait_on_ratelimit=True,  mashape_key=mashape_key,   **twitter_app_auth)
 
 for id_user, json in bom.check_accounts_in(user):
+    print('id_user')
     cursor.execute("INSERT INTO json_botometer (id_user, json_extra) VALUES (%s, %s)", [str(id_user), str(json)])
     db.commit()
+
